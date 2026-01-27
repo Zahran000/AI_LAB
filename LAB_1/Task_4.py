9bfe3e762285aa7e5962cb8588053683a78ccd99
Syed Zahran Ali Chishti <zahranc04@gmail.com>
2026-01-27 22:39:40 +0500

    Fix return statement in salary function

--- LAB_1/Task_4.py
+++ LAB_1/Task_4.py
@@ -1,7 +1,7 @@
 def salary(sal):
     HRA = sal * 0.1
     DA = sal * 0.05
-    return s + HRA + DA
+    return sal + HRA + DA
 sal = float(input("Enter basic salary: "))
 total_salary = salary(sal)
 print("Total salary:", total_salary)
