fce8e5a1de924d3295f081c6cc5b2399f7d8654a
Syed Zahran Ali Chishti <zahranc04@gmail.com>
2026-01-27 22:32:55 +0500

    Add salary calculation function

--- LAB_1/Task_4.py
+++ LAB_1/Task_4.py
@@ -1,1 +1,7 @@
-
+def salary(s):
+    HRA = s * 0.1
+    DA = s * 0.05
+    return s + HRA + DA
+s = float(input("Enter basic salary: "))
+total_salary = salary(s)
+print("Total salary:", total_salary)
