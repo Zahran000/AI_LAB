6921819964bc088d123d87d309f47f995b15e292
Syed Zahran Ali Chishti <zahranc04@gmail.com>
2026-01-27 22:29:34 +0500

    Update Task_3.py

--- LAB_1/Task_3.py
+++ LAB_1/Task_3.py
@@ -1,1 +1,28 @@
+n = int(input("Enter amount of numbers you want to enter: "))
+numbers = [0] * n
+
+for i in range(n):
+    numbers[i] = int(input(f"Enter number {i+1}: "))
+
+evens = 0
+odds = 0
+
+print("Numbers in the list:")
+for num in numbers:
+    print(num, end = ' ')
+    if num % 2 == 0:
+        evens += 1
+    else:
+        odds += 1
+
+print(f"\nEven numbers: {evens}")
+print(f"Odd numbers: {odds}")
+
+index = int(input("\nEnter index of the element you want to replace: "))
+new_value = int(input("Enter new value: "))
+numbers[index] = new_value
+
+print("\nupdated list:")
+for num in numbers:
+    print(num, end=' ')
 
