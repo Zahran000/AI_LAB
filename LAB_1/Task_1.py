9fe8f49b291efc1c27fff088c3fca5dad2236dd3
Syed Zahran Ali Chishti <zahranc04@gmail.com>
2026-01-27 20:58:41 +0500

    Update Task_1.py

--- LAB_1/Task_1.py
+++ LAB_1/Task_1.py
@@ -1,1 +1,18 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+age = int(input("Enter age: "))
+users = {
+    "username": username,
+    "password": password,
+    "age": age    
+}
+print("student information:")
+print("username: ",users["username"])
+print("password: ",users["password"])
+print("Age: ",users["age"])
 
+if age>= 13:
+    print("Account Status: Allowed")
+else:
+    print("Account Status: Not Allowed (Age < 13)")    
+    
